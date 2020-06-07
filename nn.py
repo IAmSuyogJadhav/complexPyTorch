4ec1209f4d360c63d045beb0e6a1baf96fe37b80
IAmSuyogJadhav <suyog.jadhav1@gmail.com>
2020-06-07 06:40:01 +0000

    Rearranged files to be similar to torch.nn
    - Also added AdaptiveAvgPool2D, for use with Resnet

--- nn.py
+++ nn.py
@@ -13,8 +13,39 @@ import torch
 from torch.nn import Module, Parameter, init, Sequential
 from torch.nn import Conv2d, Linear, BatchNorm1d, BatchNorm2d
 from torch.nn import ConvTranspose2d
-from complexFunctions import complex_relu, complex_max_pool2d
-from complexFunctions import complex_dropout, complex_dropout2d
+from torch.nn.functional import relu, max_pool2d, dropout, dropout2d, avg_pool2d
+
+
+def complex_relu(input_r,input_i):
+    return relu(input_r), relu(input_i)
+
+def complex_max_pool2d(input_r,input_i,kernel_size, stride=None, padding=0,
+                                dilation=1, ceil_mode=False, return_indices=False):
+
+    return max_pool2d(input_r, kernel_size, stride, padding, dilation,
+                      ceil_mode, return_indices), \
+           max_pool2d(input_i, kernel_size, stride, padding, dilation,
+                      ceil_mode, return_indices)
+
+def complex_dropout(input_r,input_i, p=0.5, training=True, inplace=False):
+    return dropout(input_r, p, training, inplace), \
+           dropout(input_i, p, training, inplace)
+
+
+def complex_dropout2d(input_r,input_i, p=0.5, training=True, inplace=False):
+    return dropout2d(input_r, p, training, inplace), \
+           dropout2d(input_i, p, training, inplace)
+
+
+
+def complex_adaptive_avg_pool2d(input_r,input_i, output_size):
+
+    """
+    Added by Suyog Jadhav
+    """
+    return adaptive_avg_pool2d(input_r, output_size), \
+           adaptive_avg_pool2d(input_i, output_size)
+
 
 class ComplexSequential(Sequential):
     def forward(self, input_r, input_t):
@@ -58,10 +89,23 @@ class ComplexMaxPool2d(Module):
                                 dilation = self.dilation, ceil_mode = self.ceil_mode,
                                 return_indices = self.return_indices)
 
+
+class ComplexAdaptiveAvgPool2d(Module):
+    """
+    Added by Suyog Jadhav
+    """
+
+    def __init__(self, output_size):
+        super(ComplexAdaptiveAvgPool2d,self).__init__()
+        self.output_size = output_size
+    def forward(self,input_r,input_i):
+        return complex_adaptive_avg_pool2d(input_r,input_i,output_size)    
+    
+
 class ComplexReLU(Module):
 
      def forward(self,input_r,input_i):
-         return complex_relu(input_r,input_i)
+        return complex_relu(input_r,input_i)
 
 class ComplexConvTranspose2d(Module):
 
